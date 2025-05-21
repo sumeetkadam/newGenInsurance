44937d8c62045dbb0ed811307c6399dc8cb0a05e
sumeetkadam <sumeetkadam22@gmail.com>
2025-05-21 17:07:55 +0530

    backend001_springboot initialized
    backend code base initialized using spring initializer with dependencies as below:
    1. Spring web
    2.Spring Data Jpa
    3.H2
    4.MySQL
    5.Lombok
    6.Spring Security
    7.Validation

---
+++ BACKEND/insurance/src/main/java/com/newgen/insurance/NewGenInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.newgen.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewGenInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewGenInsuranceApplication.class, args);
+	}
+
+}
---
+++ BACKEND/insurance/src/test/java/com/newgen/insurance/NewGenInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newgen.insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewGenInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
